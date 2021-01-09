695ac9744cd391189d68c846d3b0e05740fbee96
tataurova <tataurovaoe@yandex.ru>
2021-01-10 00:21:17 +0300

    refactor: add typesctipt environment

---
+++ src/components/app/app.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import Main from '../main/main';
+
+const App = () => <Main />;
+
+export default App;
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 
 const Main = () => (
   <div>Main component</div>

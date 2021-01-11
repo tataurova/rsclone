d24aff947962adba70df9e2c62dc3a4f62c96571
tataurova <tataurovaoe@yandex.ru>
2021-01-11 15:30:16 +0300

    fix: fix troubles with image load and css modules load

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import AccountGitHub from './accountGitHub/AccountGitHub';
+import AccountGithub from '../account-github/account-github';
 
-// import style from './Footer.module.css';
+import * as style from './footer.module.css';
+const logo =  require("../../../public/assets/img/bg.jpg");
 
-function Footer() {
+const Footer = () => {
   const styles = {
     img: {
       width: '100px',
@@ -17,7 +18,7 @@ function Footer() {
   };
   return (
       <footer
-          // className={style.footer}
+          className={style.footer}
           style={styles.footer}
       >
         <div>
@@ -26,15 +27,16 @@ function Footer() {
           <p>Settings</p>
           <p>Inform</p>
         </div>
+        <img src={ logo.default } />
         <ul
-            // className={style.account}
+            className={style.account}
         >
-          <AccountGitHub author='Olga Tataurova' address='https://github.com/tataurova/'/>
-          <AccountGitHub author='Leonid Shuliak' address='https://github.com/shuliakleonid/'/>
-          <AccountGitHub author='Andrei Padyslenkau' address='https://github.com/andreizayaz'/>
+          <AccountGithub author='Olga Tataurova' address='https://github.com/tataurova/'/>
+          <AccountGithub author='Leonid Shuliak' address='https://github.com/shuliakleonid/'/>
+          <AccountGithub author='Andrei Padyslenkau' address='https://github.com/andreizayaz'/>
         </ul>
         <div
-            // className={style.logo}
+            className={style.logo}
         >
           <a href="https://rs.school/js/" target="_blank" rel="noreferrer">
             <img style={styles.img} src='https://rs.school/images/rs_school_js.svg' alt="RS School Logo"/>
@@ -43,6 +45,6 @@ function Footer() {
         </div>
       </footer>
   );
-}
+};
 
 export default Footer;

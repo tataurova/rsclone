bef76a98833e5d2bc142917a98007f585ae00f37
Shuliak Leonid <zeleny777@gmail.com>
2021-01-15 16:33:55 +0300

    refactor component App

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import history from '../../history';
 import Main from '../main/main';
 import CardPage from '../card-page/card-page';
@@ -9,24 +9,17 @@ import { PageName } from '../../const';
 import SearchingProfile from '../search-gone/search-gone-profile';
 
 const App = () => (
-      <BrowserRouter history={history}>
+    <BrowserRouter history={history}>
         <Switch>
-          <Route exact path={PageName.MAIN} render={() => <Main />}>
-          </Route>
-          <Route exact path={PageName.GONE} render={() => <CardPage />}>
-          </Route>
-          <Route exact path={PageName.LOOKING_RELATIVES} render={() => <SearchingProfile />}>
-          </Route>
-          <Route exact path={PageName.ACTIVE_SEARCHES} render={() => <Main />}>
-          </Route>
-          <Route exact path={PageName.CLOSED_SEARCHES} render={() => <Main />}>
-          </Route>
-          <Route exact path={PageName.LOGIN} render={() => <LoginPage />}>
-          </Route>
-          <Route render={() => <NotFound />}>
-          </Route>
+            <Route exact path={PageName.MAIN} render={() => <Main/>}/>
+            <Route exact path={PageName.GONE} render={() => <CardPage/>}/>
+            <Route exact path={PageName.LOOKING_RELATIVES} render={() => <SearchingProfile/>}/>
+            <Route exact path={PageName.ACTIVE_SEARCHES} render={() => <Main/>}/>
+            <Route exact path={PageName.CLOSED_SEARCHES} render={() => <Main/>}/>
+            <Route exact path={PageName.LOGIN} render={() => <LoginPage/>}/>
+            <Route render={() => <NotFound/>}/>
         </Switch>
-      </BrowserRouter>
+    </BrowserRouter>
 );
 
 export default App;
